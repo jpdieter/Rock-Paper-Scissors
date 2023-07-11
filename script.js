324006b418363c705355291abf11a28eaba3c5b9
jpdieter <justindieter@gmail.com>
2023-07-10 17:55:03 -0700

    Rock Paper Scissors with UI

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Variables //
-//let playerScore = 0;
-//let computerScore = 0;
-let score = 0
+   
+    // Variables //
+let playerScore = 0;
+let computerScore = 0;
 
 // computer choice //
 function computerChoice(){
@@ -9,62 +9,117 @@ function computerChoice(){
     return randomChoice = computerArray[Math.floor(Math.random() * computerArray.length)];
   }
 
+  function checkForWin(){
+    if(computerScore == 5){
+        alert("You lost the game!");
+        var lose = "";
+        document.getElementById("results").innerHTML = lose;
+        resetGame();
+    }
+    if(playerScore == 5){
+        alert("You won the game!");
+        var win = "";
+        document.getElementById("results").innerHTML = win;
+        resetGame();
+    }
+  }
+
+  function resetGame(){
+    computerScore = 0;
+    playerScore = 0;
+    var compRunningScore = "";
+    document.getElementById("computerscore").innerHTML = compRunningScore;
+    var playerRunningScore = "";
+    document.getElementById("playerscore").innerHTML = playerRunningScore;
+    var computer = ("");
+    document.getElementById("computerchoice").innerHTML = computer;
+    var user = ("");
+    document.getElementById("playerchoice").innerHTML = user;
+  }
+
 // game function that loops 5 times while tracking score. if selection is invalid, it loops until a valid selection is entered. //   
 function game() {
-    selectionLoop:
-    for(let i = 0; i < 5; i++) {
-        const playerSelection = window.prompt("Enter rock, paper or scissors.").toLowerCase();
-        if((playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors") || (playerSelection == "")) {
-            i--;
-            alert("Invalid choice!")
-            continue selectionLoop;      
-        }
-        const computerSelection = computerChoice();
-        console.log("The computer chose: " + computerSelection);
-        console.log("You chose: " + playerSelection);
-        playRound(playerSelection, computerSelection);
+  //  selectionLoop:
+ // for(let i = 0; i < 5; i++) {
+    //    const playerSelection = window.prompt("Enter rock, paper or scissors.").toLowerCase();
+      //  if((playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors") || (playerSelection == "")) {
+        //    i--;
+          //  alert("Invalid choice!")
+          //  continue selectionLoop;   
+          let button = document.querySelectorAll('.choices');
+          button.forEach((btn) => {
+              btn.addEventListener("click", playRound); 
+          //const choices = document.querySelectorAll('.btn');
+         // choices.forEach((choices => choices.addEventListener('click', playRound)       playerSelection.addEventListener('click', playRound) 
+         // const playerSelection = choices.id
+        })
     }
-}
 // playRound function 
-function playRound(playerSelection, computerSelection) {
+function playRound(player) {
+    playerSelection = player.srcElement.id;
+    computerSelection = computerChoice();
+    var computer = ("The computer chose: " + computerSelection);
+    document.getElementById("computerchoice").innerHTML = computer;
+    var user = ("You chose: " + playerSelection);
+    document.getElementById("playerchoice").innerHTML = user;
+   // console.log("You chose: " + playerSelection);
     if(playerSelection == "rock") {
         if(computerSelection == "rock") {
-            console.log("You tied this round!");
+            tieRound();
         }
         else if(computerSelection == "paper") {
-            console.log("You lost this round!");
+            loseRound();
         }
         else {
-            console.log("You won this round!")
-            score++;
+            winRound();
             }
         }
         else if(playerSelection == "paper") {
             if(computerSelection == "paper") {
-                console.log("You tied this round!");
+                tieRound();
             }
             else if(computerSelection == "scissors") {
-                console.log("You lost this round!");
+                loseRound();
             }
             else {
-                console.log("You won this round!")
-                score++;
+                winRound();
         }
         }
         else {
             if(computerSelection == "scissors") {
-                console.log("You tied this round!");
+                tieRound();
             }
             else if(computerSelection == "rock") {
-                console.log("You lost this round!");
+                loseRound();
             }
             else {
-                console.log("You Won this round!")
-                score++;
+                winRound();
             }
         }
     }
 
-// console.log to print results to console
+function tieRound(){
+    var tie = 'You tied this round!';
+    document.getElementById("results").innerHTML = tie;
+}
+
+function loseRound(){
+    var lose = 'You lost this round!';
+    document.getElementById("results").innerHTML = lose;
+    computerScore++
+    var compRunningScore = "The computers score is " + computerScore ;
+    document.getElementById("computerscore").innerHTML = compRunningScore;
+    checkForWin();
+}
+
+function winRound(){
+    var win = 'You won this round!';
+    document.getElementById("results").innerHTML = win;
+    playerScore++;
+    var playerRunningScore = "Your score is " + playerScore ;
+    document.getElementById("playerscore").innerHTML = playerRunningScore;
+    checkForWin();
+}
+
+// console.log to print score to console
 game();
-console.log("Your score is " + score + "/5");
